f7d0113b5b0d46dae201324e6021dc438853e8d9
Abhishek-Hiremath49 <abhishekhiremath4949@gmail.com>
2025-09-01 16:07:12 +0530

    removed git ignore

--- mini-e-commerce/main.py
+++ mini-e-commerce/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from .database import Base, engine
-# from .routers import products, customers, orders
+from .routers import products, customers, orders
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Mini E-Commerce Backend")
 
-# app.include_router(products.router)
-# app.include_router(customers.router)
-# app.include_router(orders.router)
+app.include_router(products.router)
+app.include_router(customers.router)
+app.include_router(orders.router)
 
 @app.get("/")
 def root():
